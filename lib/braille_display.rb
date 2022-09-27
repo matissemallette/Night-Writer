d8a58aa3d7a2c7e937a22be302e8695a8b4cbafc
Matisse Mallette <matissemallette@gmail.com>
2022-09-27 13:14:47 -0700

    go a little off the rails and not really do what this branch was meant to do. basically i ended up making some helper methods for formatting text (word wrap and such) and plan on implementing those in a module on a new branch

--- lib/braille_display.rb
+++ lib/braille_display.rb
@@ -1,15 +1,62 @@
+require './lib/braille'
 
 class BrailleDisplay 
+  include Braille
+
   def initialize
-    
-  end
+    @ascii_line_length_limit = 40
+  end 
 
   def self.from_message(message)
     BrailleDisplay.new.write(message)
   end
 
+  def new_line 
+    
+  end
+
+  def tokenize(string)
+    lines = []
+    string.downcase.split("\n").map do |line| 
+      line.split.each do |word|
+        lines << word
+      end
+      lines << "\n"
+    end
+    lines[0...-1]
+  end
+
+  def word_wrap(tokens)
+    line_length = 0 
+    wrapped_tokens = []
+    self.break_up_large_tokens(tokens, @ascii_line_length_limit).each do |token|
+      line_length += token.length unless token == "\n"
+      if line_length >= @ascii_line_length_limit
+        wrapped_tokens << "\n" 
+        wrapped_tokens << token 
+        line_length = token.length
+      else
+        wrapped_tokens << token 
+      end
+    end
+    return wrapped_tokens
+  end
+
+  def break_up_large_tokens(tokens, line_length_limit)
+    broken_up_tokens = []
+    tokens.each do |token|
+      if token.length > line_length_limit
+        broken_up_tokens << token[0...line_length_limit]
+        broken_up_tokens << token[line_length_limit..token.length]
+      else  
+        broken_up_tokens << token
+      end
+    end
+    broken_up_tokens
+  end
+
   def write(message)
-    # do all the shit here 
+    p self.tokenize(message)
     self
   end
 
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,9 +4,9 @@ class NightWriter
   attr_reader :message 
   def initialize(argv)
     @message = File.read(argv[0])
-    @braille_display = BrailleDisplay.from_message(message)
+    @braille_display = BrailleDisplay.from_message(@message)
     puts @braille_display.to_s
-    File.write(argv[1], @braille_display.to_s)
+    File.write(argv[1], @message)
     puts "Created '#{argv[1]}' containing #{@message.length} characters"
   end
 end
